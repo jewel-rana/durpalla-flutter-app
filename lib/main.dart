30f784b4399903ade4139832020886fbb4e0d82c
Jewel Rana <jewelrana.dev@gmail.com>
2025-08-10 20:54:02 +0600

    Logo change

--- lib/main.dart
+++ lib/main.dart
@@ -113,7 +113,7 @@ class _MyAppState extends State<MyApp> {
       home: MainScaffold(
         appBar: AppBar(
           title: Image.asset(
-            'assets/logo.png',
+            'assets/logo-white.png',
             height: 40, // Adjust as needed
           ),
           centerTitle: true, // Center the logo
